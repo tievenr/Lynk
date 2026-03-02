7c56b024dd2bff66d58fc5f15d13af4ac75055dd
tievenr <vineetraghu17@gmail.com>
2026-03-02 13:34:17 +0530

    init: initialized repo and created a redis wrapper with a shorten url function

---
+++ app/store.py
@@ -0,0 +1,14 @@
+import redis 
+import random 
+import string 
+
+r = redis.Redis(host="redis", port=6379, decode_responses=True)
+
+def generate_code(length=6):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+def set_url(code: str, url: str):
+    r.set(code, url)
+
+def get_url(code: str):
+    return r.get(code)
